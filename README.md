385fcc3ef2eded2e36cce30bd45f4dc98604696c
Jayson Ward <defiantgoat@gmail.com>
2022-06-16 16:36:28 -0400

    created a vite version of app

--- README.md
+++
@@ -1,4 +0,0 @@
-# esri-react-typescript
-
-An esri react typescript sample
-<img width="1740" alt="Screen Shot 2021-12-13 at 10 21 39 AM" src="https://user-images.githubusercontent.com/3229911/145733741-9df4d463-d6a1-47b7-b42d-03742a08f5ad.png">
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from 'react-dom/client'
 import App from "./components/App";
 import "modern-normalize";
 
-render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App />
+)
