c397fcab2b11db4d24cca28fd8fcf7cbd45e951e
Jayson Ward <defiantgoat@gmail.com>
2022-06-22 11:49:20 -0400

    updated maptools hook

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,48 @@ export const MAP_DEFAULTS = {
   BASEMAP: "dark-gray-vector",
 };
 
-export const MAP_LAYERS = [
+
+export interface LayerConfig {
+  url: string;
+  title: string;
+  id: string;
+  type: "FeatureLayer" | "MapImageLayer" | "CVSLayer" | "GeoJSONLayer" | "OGCFeatureLayer" | "StreamLayer" | "WFSLayer";
+  renderer?: any;
+  sublayers?: {id: number, renderer?: any;}[]
+}
+
+export const MAP_LAYERS: LayerConfig[] = [
+  {
+    url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer",
+    title: "Census Blocks",
+    id: "census_blocks_map",
+    type: "MapImageLayer",
+    sublayers: [{
+      id: 2,
+      renderer: {
+        type: "simple",
+        symbol: {
+          "type": "simple-fill",
+          color: [255, 0, 255, 0.1],
+          outline: null
+        }
+      }
+    },
+    {
+      id: 0,
+      renderer: {
+        type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+        style: "square",
+        color: "blue",
+        size: "8px",  // pixels
+        outline: {  // autocasts as new SimpleLineSymbol()
+          color: [ 255, 255, 0 ],
+          width: 3  // points
+        }
+      }
+    }
+  ]
+  },
   {
     url: "https://services1.arcgis.com/FNsEJ848HT5uDOHD/ArcGIS/rest/services/TrailsMV_Open_Space_Data_pv/FeatureServer/0",
     title: "Cons Areas",
@@ -16,6 +57,15 @@ export const MAP_LAYERS = [
     title: "Trails",
     id: "trails",
     type: "FeatureLayer",
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "simple-line",
+        color: "#555",
+        width: "3px",
+        style: "short-dot"
+      }
+    }
   },
   {
     url: "https://services1.arcgis.com/FNsEJ848HT5uDOHD/ArcGIS/rest/services/TrailsMV_POIs_pv/FeatureServer/0",
--- src/hooks/useMapTools.ts
+++ src/hooks/useMapTools.ts
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import MapContext from "../components/MapContext";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import MapImageLayer from "@arcgis/core/layers/MapImageLayer";
 import Layer from "@arcgis/core/layers/Layer";
+import Renderer from "@arcgis/core/renderers/Renderer";
+import { LayerConfig } from "../config";
 
 const useMapTools = () => {
   const mapViewContext = useContext(MapContext) as any;
 
-  const _findLayer = (id: string): Layer | null => mapViewContext?.map.allLayers.find((layer) => layer.id === id);
+  const findLayer = (id: string): Layer | null => mapViewContext?.map.allLayers.find((layer: Layer) => layer.id === id);
 
   const changeBasemap = (basemap: string): void => {
     if (mapViewContext) {
@@ -14,13 +17,8 @@ const useMapTools = () => {
     }
   };
 
-  const addLayer = (layerConfig: {
-    url: string;
-    title: string;
-    type: string;
-    id: string;
-  }) => {
-      const { url, title, type, id } = layerConfig;
+  const addLayer = (layerConfig: LayerConfig) => {
+      const { url, title, type, id, renderer, sublayers } = layerConfig;
       if (type === "FeatureLayer") {
         const featureLayer = new FeatureLayer({
           url,
@@ -28,21 +26,38 @@ const useMapTools = () => {
           title,
         });
 
+        if (renderer) {
+          featureLayer.renderer = renderer;
+        }
+
         mapViewContext?.map.add(featureLayer);
+      }
+      if (type === "MapImageLayer") {
+        const mapImageLayer = new MapImageLayer({
+          url,
+          id,
+          title,
+        });
+
+        if (sublayers) {
+          mapImageLayer.sublayers = sublayers as any;
+        }
+
+        mapViewContext?.map.add(mapImageLayer);
       }
   };
 
   const removeLayer = (id: string) => {};
 
   const hideLayer = (id: string) => {
-    const layer = _findLayer(id);
+    const layer = findLayer(id);
     if (layer) {
       layer.visible = false;
     }
   };
 
   const showLayer = (id: string) => {
-    const layer = _findLayer(id);
+    const layer = findLayer(id);
     if (layer) {
       layer.visible = true;
     }
@@ -54,6 +69,7 @@ const useMapTools = () => {
     removeLayer,
     hideLayer,
     showLayer,
+    findLayer
   };
 };
 
